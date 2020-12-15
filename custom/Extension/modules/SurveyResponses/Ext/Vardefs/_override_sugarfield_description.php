4aef99e2186db12ec624d64d164526f6a8a9c2b0
owl-mark <mark_huang@owlting.com>
2020-12-15 15:27:20 +0800

    add audited to survey responses module

---
+++ custom/Extension/modules/SurveyResponses/Ext/Vardefs/_override_sugarfield_description.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2020-12-15 07:25:19
+$dictionary['SurveyResponses']['fields']['description']['audited']=true;
+$dictionary['SurveyResponses']['fields']['description']['inline_edit']=true;
+$dictionary['SurveyResponses']['fields']['description']['comments']='Full text of the note';
+$dictionary['SurveyResponses']['fields']['description']['merge_filter']='disabled';
+
+ ?>
---
+++ custom/Extension/modules/SurveyResponses/Ext/Vardefs/_override_sugarfield_email_response_sent.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2020-12-15 07:26:12
+$dictionary['SurveyResponses']['fields']['email_response_sent']['default']='0';
+$dictionary['SurveyResponses']['fields']['email_response_sent']['audited']=true;
+$dictionary['SurveyResponses']['fields']['email_response_sent']['inline_edit']=true;
+$dictionary['SurveyResponses']['fields']['email_response_sent']['merge_filter']='disabled';
+
+ ?>

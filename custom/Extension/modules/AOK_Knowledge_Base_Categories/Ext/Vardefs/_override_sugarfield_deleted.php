88ff16db9457ef20f046123225ada2d1e148dee4
owl-mark <mark_huang@owlting.com>
2020-12-15 17:48:34 +0800

    add audited to kb categories module

---
+++ custom/Extension/modules/AOK_Knowledge_Base_Categories/Ext/Vardefs/_override_sugarfield_deleted.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2020-12-15 09:47:56
+$dictionary['AOK_Knowledge_Base_Categories']['fields']['deleted']['audited']=true;
+$dictionary['AOK_Knowledge_Base_Categories']['fields']['deleted']['inline_edit']=true;
+$dictionary['AOK_Knowledge_Base_Categories']['fields']['deleted']['comments']='Record deletion indicator';
+$dictionary['AOK_Knowledge_Base_Categories']['fields']['deleted']['merge_filter']='disabled';
+$dictionary['AOK_Knowledge_Base_Categories']['fields']['deleted']['reportable']=true;
+
+ ?>
---
+++ custom/modules/AOK_Knowledge_Base_Categories/language/en_us.lang.php
@@ -0,0 +1,7 @@
+<?php
+// created: 2020-12-15 09:47:56
+$mod_strings = array (
+  'LBL_NAME' => 'Name',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_DELETED' => 'Deleted',
+);

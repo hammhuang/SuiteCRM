bf75abcf0ea4e0a827c4bc7eb773b558b6ad7f4c
owl-mark <mark_huang@owlting.com>
2020-12-15 15:29:46 +0800

    add audited to survey questions module

---
+++ custom/Extension/modules/SurveyQuestions/Ext/Vardefs/_override_sugarfield_sort_order.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2020-12-15 07:28:01
+$dictionary['SurveyQuestions']['fields']['sort_order']['audited']=true;
+
+ ?>
---
+++ custom/Extension/modules/SurveyQuestions/Ext/Vardefs/_override_sugarfield_type.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2020-12-15 07:28:22
+$dictionary['SurveyQuestions']['fields']['type']['audited']=true;
+
+ ?>

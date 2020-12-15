689f0bbb6f702852c2dd6d936cd59e0b1ca4340b
owl-mark <mark_huang@owlting.com>
2020-12-15 17:50:44 +0800

    add audited to reports module

---
+++ custom/Extension/modules/AOR_Reports/Ext/Vardefs/_override_sugarfield_description.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2020-12-15 09:50:01
+$dictionary['AOR_Report']['fields']['description']['audited']=true;
+$dictionary['AOR_Report']['fields']['description']['inline_edit']=true;
+$dictionary['AOR_Report']['fields']['description']['comments']='Full text of the note';
+$dictionary['AOR_Report']['fields']['description']['merge_filter']='disabled';
+
+ ?>

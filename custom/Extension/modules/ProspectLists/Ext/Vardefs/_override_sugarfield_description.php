6fc98a201d05ab429ae3419473196b38765fc7f9
owl-mark <mark_huang@owlting.com>
2020-12-15 10:11:02 +0800

    add audited to target lists module

---
+++ custom/Extension/modules/ProspectLists/Ext/Vardefs/_override_sugarfield_description.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2020-12-14 10:28:47
+$dictionary['ProspectList']['fields']['description']['audited']=true;
+$dictionary['ProspectList']['fields']['description']['inline_edit']=true;
+$dictionary['ProspectList']['fields']['description']['merge_filter']='disabled';
+$dictionary['ProspectList']['fields']['description']['rows']='4';
+$dictionary['ProspectList']['fields']['description']['cols']='20';
+
+ ?>
---
+++ custom/Extension/modules/ProspectLists/Ext/Vardefs/_override_sugarfield_list_type.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2020-12-14 10:28:19
+$dictionary['ProspectList']['fields']['list_type']['audited']=true;
+$dictionary['ProspectList']['fields']['list_type']['inline_edit']=true;
+$dictionary['ProspectList']['fields']['list_type']['merge_filter']='disabled';
+
+ ?>
